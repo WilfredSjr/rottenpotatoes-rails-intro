f8a002e8c56e0e69613e534db39f0d1d7cdb39b1
Wilfred Smith <wsmithjr@uccs.edu>
2019-02-25 04:38:29 +0000

    hw2 final test2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -15,7 +15,7 @@ class MoviesController < ApplicationController
 
     #Initial setting up of sessions
     session[:ratings] ||= @all_ratings
-    session[:sort_by] ||= "id"
+    session[:sort_by] ||= 'id'
 
     @title_hilite = session[:title_hilite] = 'hilite' if params[:sort_by] == 'title'
     @release_date_hilite = session[:release_date_hilite] = 'hilite' if params[:sort_by] == 'release_date'

2ef984159eee76b4a0f900222de56d4e0cd01a4a
Wilfred Smith <wsmithjr@uccs.edu>
2019-02-25 03:26:27 +0000

    part 2 test 5

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,16 +11,15 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @all_ratings = Movie.order(:rating).select(:rating).map(&:rating).uniq
-    @checked_ratings = check
-    @checked_ratings.each do |rating|
-      params[rating] = true
-  end
-  
-  if params[:sort_by]
-      @movies = Movie.order(params[:sort_by])
-    else
-      @movies = Movie.where(:rating => @checked_ratings)
+    @all_ratings = Movie.all_ratings
+    @sort_by = params[:sort_by] || session[:sort_by]
+    @ratings = params[:ratings]  || session[:ratings] || all_ratings
+    @movies = Movie.where( { rating: @ratings.keys } ).order(@sort_by)
+    session[:sort], session[:ratings] = @sort_by, @ratings
+    
+     if params[:sort_by] != session[:sort_by] or params[:ratings] != session[:ratings]
+      flash.keep
+      redirect_to movies_path sort_by: @sort_by, ratings: @ratings
     end
   end
   
